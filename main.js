e0e226abeaa0c3fa06483967e81202ddc7d6fb2a
Desmondflexy <ikilodesmond@gmail.com>
2023-01-25 01:46:53 +0100

    final update

--- main.js
+++ main.js
@@ -22,10 +22,13 @@ for (let i = 0; i < 2; i++) {  // two players
 
 const card_suits = ['S', 'H', 'D', 'C'];  // spade, heart, diamond, club
 const card_values = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
-const deck_of_cards = [];
+let deck_of_cards = [];
 card_values.forEach(val => card_suits.forEach(suit => deck_of_cards.push(val + suit)));
 const card_unicodes = { 'cover': '&#127136;' };
+/**Copy of the full deck of cards before game */
+const DeckOfCards = deck_of_cards.map(i => i);
 
+// Asign standard unicode to each card.
 let jj = 0;
 for (let j = 10; j <= 13; j++) {
     let ii = 0;
@@ -40,7 +43,7 @@ for (let j = 10; j <= 13; j++) {
 }
 
 /**Deals a random card to player from the deck of cards */
-function deal_a_card(player) {
+function deal_a_card(player = []) {
     const card = deck_of_cards[getRandomInt(0, deck_of_cards.length - 1)];
     const pos = deck_of_cards.indexOf(card);
     player.push(...deck_of_cards.splice(pos, 1));
@@ -116,31 +119,21 @@ function handvalue(hand) {
     return lst.map(i => i).sort((x, y) => y - x);
 }
 
-// Global variables declaration
-let r1, r2;
-
-/**Two players play a  game of poker */
-function play_poker() {
-    // Random hands dealt to two players
-    const player_1 = [];
-    const player_2 = [];
-    for (let i = 0; i < 5; i++) {
-        deal_a_card(player_1);
-        deal_a_card(player_2)
-    }
-    r1 = rank(player_1)[0];
-    r2 = rank(player_2)[0];
+/**Compare two hands in a poker game */
+function poker(hand1, hand2) {
+    const r1 = rank(hand1);
+    const r2 = rank(hand2);
 
     let result
-    if (r1 > r2) result = 'player1_wins (rank)';
-    else if (r1 < r2) result = 'player2_wins (rank)';
+    if (r1[0] > r2[0]) result = 'player1 wins (rank)';
+    else if (r1[0] < r2[0]) result = 'player2 wins (rank)';
     else {
-        const p1 = handvalue(player_1);
-        const p2 = handvalue(player_2);
+        const p1 = handvalue(hand1);
+        const p2 = handvalue(hand2);
 
-        if (r1 < 2) {
-            if (arrayCompare(p1, p2) > 0) result = 'player1_wins (highest_card)';
-            else if (arrayCompare(p1, p2) < 0) result = 'player2_wins (highest_card)';
+        if (r1[0] < 2) {
+            if (arrayCompare(p1, p2) > 0) result = 'player1 wins (highest card)';
+            else if (arrayCompare(p1, p2) < 0) result = 'player2 wins (highest card)';
             else result = 'draw';
         } else {
             const a = p1.map(i => count(p1, i));
@@ -148,57 +141,33 @@ function play_poker() {
             const b = p2.map(i => count(p2, i));
             const n2 = p2[b.indexOf(Math.max(...b))];
 
-            if (n1 > n2) result = 'player1_wins (high_value_rank)';
-            else if (n1 < n2) result = 'player2_wins (high_value_rank)';
+            if (n1 > n2) result = 'player1 wins (high value rank)';
+            else if (n1 < n2) result = 'player2 wins (high value rank)';
             else {
                 for (let i = 0; i < Math.max(...a); i++) {
                     p1.splice(p1.indexOf(n1), 1);
                     p2.splice(p2.indexOf(n2), 1);
                 }
-                if (arrayCompare(p1, p2) > 0) result = 'player1_wins (tie__highest_cards)';
-                else if (arrayCompare(p1, p2) < 0) result = 'player2_wins (tie__highest_cards)';
+                if (arrayCompare(p1, p2) > 0) result = 'player1 wins (tie, highest cards)';
+                else if (arrayCompare(p1, p2) < 0) result = 'player2 wins (tie, highest cards)';
                 else result = 'draw';
             }
         }
     }
-    // print result to console
-    if ((r1 > 0 || r2 > 0) && (result.includes(''))) {
-        console.log(`[${player_1}] ${r1} -- ${r2} [${player_2}] -- ${result}`);
+    return_cards();
+
+    return {
+        // 'hand_rank': [r1, r2],
+        'rank1': r1[0], 'rank2': r2[0],
+        'rank1_name': r1[1], 'rank2_name': r2[1],
+        'result': result,
+        'full_info': `[${hand1}] ${r1[0]} -- ${r2[0]} [${hand2}] -- ${result}`
     }
 
-    const players = [player_1, player_2];
-    for (let i = 0; i < 2; i++) {  // player
-        for (let j = 0; j < 5; j++) {  // card
-            const card_div = card_array[i][j];
-            const card = players[i][j];
-            card_div.innerHTML = card_unicodes[card];
-            if (card.includes('D') || card.includes('H')) {
-                card_div.className = 'red card';
-            } else card_div.className = 'black card';
-        }
-        rank_divs[i].innerHTML = rank(players[i])[1];
-    }
-
-    if (result.includes('player1')) {
-        p1_span.innerHTML = 'wins';
-        p2_span.innerHTML = 'loses';
-        p1_span.parentElement.parentElement.className = 'player winner';
-        p2_span.parentElement.parentElement.className = 'player loser';
-    } else if (result.includes('player2')) {
-        p1_span.innerHTML = 'loses';
-        p2_span.innerHTML = 'wins';
-        p1_span.parentElement.parentElement.className = 'player loser';
-        p2_span.parentElement.parentElement.className = 'player winner';
-    } else {
-        p1_span.innerHTML = p2_span.innerHTML = 'draws';
-    }
-
-    // Both players drop their cards back on the deck
-    deck_of_cards.push(...player_1, ...player_2);
-    player_1.splice(0);
-    player_2.splice(0);
 }
 
+/**Return all cards back to deck. */
+const return_cards = () => deck_of_cards = [...DeckOfCards];
 
 /**Returns the number of occurence of an element in an array */
 const count = (arr, elem) => arr.filter(i => i === elem).length;
@@ -220,18 +189,53 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-// button click will deal a card
-function new_deal() {
-    for (let i = 0; i < 1; i++) {
-        // clear html elements before restart
-        p1_span.innerHTML = p2_span.innerHTML = '';
-        if (true) {
-            play_poker();
+function createPokerHtml(hand1, hand2, result, r1, r2) {
+    const players = [hand1, hand2];
+    const r = [r1, r2];
+    for (let i = 0; i < 2; i++) {  // player
+        for (let j = 0; j < 5; j++) {  // card
+            const card_div = card_array[i][j];
+            const card = players[i][j];
+            card_div.innerHTML = card_unicodes[card];
+            if (card.includes('D') || card.includes('H')) {
+                card_div.className = 'red card';
+            } else card_div.className = 'black card';
         }
+        rank_divs[i].innerHTML = r[i];
+    }
+
+    if (result.includes('player1')) {
+        p1_span.innerHTML = 'wins';
+        p2_span.innerHTML = 'loses';
+        p1_span.parentElement.parentElement.className = 'player winner';
+        p2_span.parentElement.parentElement.className = 'player loser';
+    } else if (result.includes('player2')) {
+        p1_span.innerHTML = 'loses';
+        p2_span.innerHTML = 'wins';
+        p1_span.parentElement.parentElement.className = 'player loser';
+        p2_span.parentElement.parentElement.className = 'player winner';
+    } else {
+        p1_span.innerHTML = p2_span.innerHTML = 'draws';
     }
 }
 
-button.onclick = new_deal;
+function new_deal() {
+    // clear html elements before restart
+    p1_span.innerHTML = p2_span.innerHTML = '';
+    const hand1 = [];
+    const hand2 = [];
+    for (let i = 0; i < 5; i++) {
+        deal_a_card(hand1);
+        deal_a_card(hand2)
+    }
+    const data = poker(hand1, hand2);
+    return [data, hand1, hand2];
+}
+
+button.addEventListener('click', () => {
+    const data = new_deal();
+    createPokerHtml(data[1], data[2], data[0]['result'], data[0]['rank1_name'], data[0]['rank2_name']);
+})
 
 //*********************************************** */
 
